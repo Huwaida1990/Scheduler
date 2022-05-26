f9cf6c605626bb8dc605c6a189000903ca5aa011
Huwaida1990 <huwaida_khaild@yahoo.com>
2022-05-26 12:30:43 -0600

    get days request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,32 +1,8 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
 const appointments = {
   "1": {
     id: 1,
@@ -67,7 +43,15 @@ const appointments = {
 };
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    let url = 'http://localhost:8001/api/days';
 
+    axios.get(url).then((response) => {
+      console.log(response);
+      setDays(response.data)
+    })
+  }, [])
   const appoin = () => {
     let result = []
     Object.values(appointments).map((appointment) => {
