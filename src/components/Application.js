2a6042ce9d5150191fe1b57996caaed5c6012148
Huwaida1990 <huwaida_khaild@yahoo.com>
2022-06-13 16:30:22 -0600

    fix bug

--- src/components/Application.js
+++ src/components/Application.js
@@ -57,6 +57,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {appointments}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
